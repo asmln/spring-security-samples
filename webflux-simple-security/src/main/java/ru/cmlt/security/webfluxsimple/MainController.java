a58c163ec87224e6f071d1a491010acf94108178
samoylenkoa <samoylenkoa@cmlt.ru>
2018-05-21 11:44:49 +0300

    webflux-simple: change return message text.

--- webflux-simple-security/src/main/java/ru/cmlt/security/webfluxsimple/MainController.java
+++ webflux-simple-security/src/main/java/ru/cmlt/security/webfluxsimple/MainController.java
@@ -18,21 +18,21 @@ public class MainController {
         return principalM
                 .map(Principal::getName)
                 .defaultIfEmpty("anon")
-                .map(name -> String.format("Welcome, %s!", name));
+                .map(name -> String.format("webflux-simple -> Welcome, %s!", name));
     }
 
     @GetMapping("/profile")
     public Mono<String> profile(Mono<Principal> principalM) {
         return principalM
                 .map(Principal::getName)
-                .map(name -> String.format("Profile: %s", name));
+                .map(name -> String.format("webflux-simple -> Profile: %s", name));
     }
 
     @GetMapping("/admin")
     public Mono<String> greetAdmin(Mono<Principal> principalM) {
         return principalM
                 .map(Principal::getName)
-                .map(name -> String.format("Admin access: %s", name));
+                .map(name -> String.format("webflux-simple -> Admin access: %s", name));
     }
 
 }
